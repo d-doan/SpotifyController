6d81d3d6548c98205b08e23675ce30990c36d72c
d-doan <dandoan03@gmail.com>
2023-10-03 21:50:35 -0700

    skeleton arduino code

---
+++ SpotifyController.ino
@@ -0,0 +1,18 @@
+
+const int playPauseButton = 9;
+const int nextButton = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(playPauseButton, INPUT_PULLUP);
+  pinMode(nextButton, INPUT_PULLUP);
+
+  // can't use Keyboard.h since using arduino Uno
+  // need to find workaround
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
