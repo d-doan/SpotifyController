a69c2666c90147e6174625a73ddfcf15d111ad09
d-doan <dandoan03@gmail.com>
2023-10-07 21:47:54 -0700

    Add artist name under title name

--- SpotifyCommunication.py
+++ SpotifyCommunication.py
@@ -10,39 +10,42 @@ CLIENT_SECRET = config('SPOTIPY_CLIENT_SECRET')
 REDIRECT_URI = config('SPOTIPY_REDIRECT_URI')
 BAUD_RATE = 9600
 
+# establish connection to arduino
+ser = serial.Serial("COM3", BAUD_RATE)
+
+def close_serial():
+    ser.close()
+    print("Serial connection closed!")
+
+atexit.register(close_serial)
+
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                client_secret=CLIENT_SECRET,
                                                redirect_uri=REDIRECT_URI,
                                                scope="user-modify-playback-state user-read-playback-state"))
 
-# establish connection to arduino
-ser = serial.Serial("COM    3", BAUD_RATE)
-
 last_track_name = ""
-
-# def close_serial():
-#     ser.close()
-#     print("Serial connection closed!")
-
-# atexit.register(close_serial)
 
 while True:
 
     current_playback = sp.current_playback()
 
-    if current_playback:
+    if current_playback and current_playback['item']:
         track_name = current_playback['item']['name']
 
+        # need to combine all artists together into string
+        artist_name = ', '.join([artist['name'] for artist in current_playback['item']['artists']])
+
+        combined_track_artist = track_name + "|" + artist_name
         if track_name != last_track_name:
-            ser.write(track_name.encode())
+            ser.write(combined_track_artist.encode())
             last_track_name = track_name
 
     if ser.inWaiting():
         command = ser.readline().decode("utf-8").strip()
 
-        if command == "PLAY_PAUSE" and current_playback:
-            # current_playback = sp.current_playback()
-            if current_playback['is_playing']:
+        if command == "PLAY_PAUSE":
+            if current_playback and current_playback['is_playing']:
                 sp.pause_playback()
             else:
                 sp.start_playback()
@@ -53,7 +56,6 @@ while True:
 
 # implement
 # setup spotify api call to get song name
-# poke around see if there is anything else cool
 # possibly rename file
 # work with display
 # maybe integrate spotify api for specific functionality
@@ -61,5 +63,3 @@ while True:
 # also add ability to add song into liked playlist
 # shuffle between list of predetermined playlists?
 # use joystick to go seek in song
-
-# TODO add close port upon script close
--- SpotifyController.ino
+++ SpotifyController.ino
@@ -13,7 +13,6 @@ const int playPauseButton = 9;
 const int nextButton = 3;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(playPauseButton, INPUT_PULLUP);
   pinMode(nextButton, INPUT_PULLUP);
@@ -27,20 +26,42 @@ void setup() {
 
 }
 
-void writeSongTitle(String title) {
+// calculates display center given song title to write to
+int getCenteredXCoord(String text) {
+  int16_t x1, y1;
+  uint16_t w, h;
+  // get the boundary of the text
+  display.getTextBounds(text, 0, 0, &x1, &y1, &w, &h); 
+
+  return (display.width() - w) / 2;
+}
+
+// writes given title to display
+void writeSongTitle(String combinedSong) {
+  int separatorIndex = combinedSong.indexOf('|');
+  String title = combinedSong.substring(0, separatorIndex);
+  String artist = combinedSong.substring(separatorIndex + 1);
+
   display.clearDisplay();
-  display.setTextSize(1);  // Set text size. Increase for larger text.
-  display.setTextColor(SSD1306_WHITE);  // Set text color
-  display.setCursor(0,0);  // Start at top-left corner
+  display.setTextSize(2);  
+  display.setTextColor(SSD1306_WHITE);
+
+  int titleX = getCenteredXCoord(title);
+  display.setCursor(titleX, 0);  // 10 is an arbitrary y-coordinate, adjust as needed
   display.println(title);
+
+  display.setTextSize(1); 
+  int artistX = getCenteredXCoord(artist);
+  display.setCursor(artistX, 32);  // 30 is an arbitrary y-coordinate, adjust based on title's display height
+  display.println(artist);
+
   display.display();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
   if (Serial.available()) {
-    String songTitle = Serial.readStringUntil('\n');  // Read the incoming data until a newline is received
+    // Read the incoming data until a newline is received
+    String songTitle = Serial.readStringUntil('\n');
     writeSongTitle(songTitle);
   }
 
