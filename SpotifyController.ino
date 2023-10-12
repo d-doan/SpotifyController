7bb7b7481af20b608b17097d684e913452ace312
d-doan <dandoan03@gmail.com>
2023-10-11 23:00:02 -0700

    Implemented marquee scrolling

--- SpotifyController.ino
+++ SpotifyController.ino
@@ -4,26 +4,28 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
+// Pin locations for wires
+#define PLAY_PAUSE_BUTTON 9
+#define NEXT_BUTTON 3
 
 #define OLED_ADDR   0x3C
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
 
-const int playPauseButton = 9;
-const int nextButton = 3;
-
 // Number of pixels to shift the text to the left each frame
 const int SCROLL_SPEED = 1; 
 int scrollPosition; 
+const int CHAR_WIDTH = 12;
+const int MAX_CHAR_DISPLAY_LENGTH = 120;          // Last legal char for a given row given display width is 128
 
 String currentSongTitle = "";
-unsigned long lastButtonPressTime = 0;
-const unsigned long BUTTON_DEBOUNCE_DELAY = 500; // Time in milliseconds to debounce button presses
-const unsigned long HEARTBEAT_TIMEOUT = 600000;
+unsigned long lastButtonPressTime = 0;    
+const unsigned long BUTTON_DEBOUNCE_DELAY = 500;  // Time in milliseconds to debounce button presses
+const unsigned long HEARTBEAT_TIMEOUT = 600000;   // Time before screen turns off without receiving data
 unsigned long lastHeartbeat = 0;
 bool isDisplayActive = true;
 
-// calculates display center given song title to write to
+// Calculates display center given song title to write to
 int getCenteredXCoord(String text) {
   int16_t x1, y1;
   uint16_t w, h;
@@ -31,7 +33,7 @@ int getCenteredXCoord(String text) {
   return (display.width() - w) / 2;
 }
 
-// calculate text width and set text size
+// Calculate text width and set text size
 int getTextWidth(String text, uint8_t textSize) {
   display.setTextSize(textSize);
   int16_t x1, y1;
@@ -41,37 +43,45 @@ int getTextWidth(String text, uint8_t textSize) {
   return w;
 }
 
-// writes given title to display
+// Writes to display and implements marquee scrolling
 void writeSongTitle(String combined) {
     int separatorIndex = combined.indexOf('|');
     String title = combined.substring(0, separatorIndex);
     String artist = combined.substring(separatorIndex + 1);
 
     display.clearDisplay();
-
-    // Set text size to 2 for the title
     display.setTextSize(2);
     display.setTextColor(SSD1306_WHITE);
 
     int titleWidth = getTextWidth(title, 2);
-    // titleWidth truncates at screen size 128, char size is 12px
-    // therefore need to check for titleWidth being 120 too
-    if (titleWidth > SCREEN_WIDTH || titleWidth == 120) {
-      // If we've scrolled off the end, reset scrollPosition
-      if (scrollPosition + titleWidth <= 0) {
-          scrollPosition = SCREEN_WIDTH;
-      }
-      display.setCursor(scrollPosition, 0);
-      display.print(title);
-      scrollPosition -= SCROLL_SPEED;
+
+    if (titleWidth == MAX_CHAR_DISPLAY_LENGTH || titleWidth > SCREEN_WIDTH) {
+        // Reset the scroll position if necessary
+        if (scrollPosition <= -titleWidth) {
+            scrollPosition = SCREEN_WIDTH - CHAR_WIDTH;
+        }
+
+        // Determine which characters are currently visible on the screen
+        int startIndex = max(0, -(scrollPosition / CHAR_WIDTH));
+        int endIndex = min(title.length(), startIndex + (SCREEN_WIDTH / CHAR_WIDTH) + 1);
+        
+        int x = scrollPosition;
+        for (int i = startIndex; i < endIndex; i++) {
+            if (x + CHAR_WIDTH <= SCREEN_WIDTH) {  // Make sure the character fits on the display
+                display.setCursor(x, 0);
+                display.print(title[i]);
+                x += CHAR_WIDTH; // Advance for the next character
+            }
+        }
+        scrollPosition -= SCROLL_SPEED;
     } else {
-        // If the title fits on the screen, center it
+        // If the title fits on the screen, simply center it
         int titleX = getCenteredXCoord(title);
         display.setCursor(titleX, 0);
-        display.println(title);
+        display.print(title);
     }
 
-    // Set text size to 1 for the artist 
+    // Handle the artist as before
     display.setTextSize(1);
     int artistX = getCenteredXCoord(artist);
     display.setCursor(artistX, 32);
@@ -82,8 +92,8 @@ void writeSongTitle(String combined) {
 
 void setup() {
   Serial.begin(9600);
-  pinMode(playPauseButton, INPUT_PULLUP);
-  pinMode(nextButton, INPUT_PULLUP);
+  pinMode(PLAY_PAUSE_BUTTON, INPUT_PULLUP);
+  pinMode(NEXT_BUTTON, INPUT_PULLUP);
 
   // Initialize OLED display
   if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
@@ -92,7 +102,7 @@ void setup() {
   }
   display.clearDisplay();
 
-  scrollPosition = SCREEN_WIDTH;
+  scrollPosition = SCREEN_WIDTH - CHAR_WIDTH;
 }
 
 void loop() {
@@ -105,7 +115,7 @@ void loop() {
         lastHeartbeat = millis();
     } else if (received != "PLAY_PAUSE" && received != "NEXT_TRACK") {
         if (currentSongTitle != received) {
-          scrollPosition = SCREEN_WIDTH - 12;
+          scrollPosition = SCREEN_WIDTH - CHAR_WIDTH;
         }
         // Update song title if not a command
         currentSongTitle = received;
@@ -128,15 +138,15 @@ void loop() {
   // Non-blocking check for button presses using debounce technique
   unsigned long currentMillis = millis();
   
-  if (digitalRead(playPauseButton) == LOW && currentMillis - lastButtonPressTime > BUTTON_DEBOUNCE_DELAY) {
+  if (digitalRead(PLAY_PAUSE_BUTTON) == LOW && currentMillis - lastButtonPressTime > BUTTON_DEBOUNCE_DELAY) {
     Serial.println("PLAY_PAUSE");
     lastButtonPressTime = currentMillis;
   } 
-  else if (digitalRead(nextButton) == LOW && currentMillis - lastButtonPressTime > BUTTON_DEBOUNCE_DELAY) {
+  else if (digitalRead(NEXT_BUTTON) == LOW && currentMillis - lastButtonPressTime > BUTTON_DEBOUNCE_DELAY) {
     Serial.println("NEXT_TRACK");
     lastButtonPressTime = currentMillis;
   }
 
-  // regulates display refresh rate
+  // Regulate display refresh rate
   delay(50);
 }
